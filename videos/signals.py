2cb7da3d51d0b03ef5815fd74ce20e9b330389c6
SunnyDevZH <yannick.vaterlaus@windowslive.com>
2025-07-17 15:50:43 +0200

    chore: update .env.template and clean up signals.py; add settings module and remove redundant comments

--- videos/signals.py
+++ videos/signals.py
@@ -1,19 +1,13 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django_rq import get_queue
 
 from .models import Video
 from .tasks import generate_resolutions
 
 @receiver(post_save, sender=Video)
 def trigger_resolution_generation(sender, instance, created, **kwargs):
-    """
-    Signal handler that triggers video resolution generation after a new video is saved.
-    - Only triggers if a new Video instance is created and has an original video file.
-    - Enqueues the generate_resolutions task in the default queue.
-    """
-    # Nur auslösen, wenn ein neues Video erstellt wurde und originalvideo vorhanden
     if created and instance.video_file:
+        from django_rq import get_queue 
         queue = get_queue('default')
         queue.enqueue(generate_resolutions, instance.id)
         print(f"Enqueued generate_resolutions für Video {instance.id}")
